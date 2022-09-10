98f86df267a6fddd414d053bc92592ac2a3d5c90
lifeisegg123 <leejj2002@naver.com>
2022-09-10 12:21:08 +0900

    change: esbuild setup

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,20 +4,23 @@ const esbuild = require("esbuild");
 const options = {
   entryPoints: ["./src/index.ts"],
   minify: true,
+  target: "es2015",
   external: ["react"],
   sourcemap: true,
   outdir: "./dist",
   bundle: true,
 };
 
-esbuild.build({
-  ...options,
-  format: "cjs",
-});
-esbuild.build({
-  ...options,
-  format: "esm",
-  outExtension: {
-    ".js": ".mjs",
-  },
-});
+Promise.all([
+  esbuild.build({
+    ...options,
+    format: "cjs",
+  }),
+  esbuild.build({
+    ...options,
+    format: "esm",
+    outExtension: {
+      ".js": ".mjs",
+    },
+  }),
+]).catch(() => process.exit(1));
