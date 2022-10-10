e6beeffbe09dc40b41e0f2e3d4cb17da39166135
lifeisegg <leejj2002@naver.com>
2022-10-11 01:11:42 +0900

    fix: remove unnecessary error throw (#21)

--- packages/react/src/useReastorageMachine.ts
+++ packages/react/src/useReastorageMachine.ts
@@ -4,9 +4,6 @@ import { useSyncExternalStore } from "use-sync-external-store/shim/index.js";
 export const useReastorageMachine = <T, A, M>(
   storage: ReastorageMachine<T, A, M>
 ) => {
-  if (!storage.getMachineState())
-    throw new Error(`[Reastorage] storage ${storage.key} has no machine`);
-
   return [
     useSyncExternalStore(
       storage.subscribeMachineState,
